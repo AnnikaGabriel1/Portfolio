8260fee1114189dde40d74118083f701a98f8955
Annika Gabriel <annika.gabriel@live.de>
2025-02-04 15:52:33 +0100

    Fixed Media-Paths

--- src/workfield.js
+++ src/workfield.js
@@ -4,7 +4,7 @@ export default [
 		title: 'UI Design',
 		description:
 			"User interfaces should be intuitive and clear. I combine visual design with well thoughtful functionality to ensure smooth interaction. Whether it's a complex application or a minimalist interface, every element is designed to lead the user effortlessly to their goal.",
-		img: '../graphics/Illustration_UI.svg',
+		img: '/graphics/Illustration_UI.svg',
 		alt: 'Illustration ',
 	},
 	{
@@ -12,7 +12,7 @@ export default [
 		title: 'UX Strategy',
 		description:
 			'Behind every good design is a well thought-out strategy. I analyse user needs, identify challenges and develop well-founded concepts that benefit users and brands alike. My UX strategies combine creativity and functionality to create digital experiences.',
-		img: '../graphics/Illustration_UX.svg',
+		img: '/graphics/Illustration_UX.svg',
 		alt: 'Illustration',
 	},
 	{
@@ -20,7 +20,7 @@ export default [
 		title: 'Branding Design & Strategy',
 		description:
 			'Strong branding is more than just a logo - it tells a story. I help brands to sharpen their identity and develop a visual language that appeals to exactly the right target groups. From conception to implementation, I provide support with designs and strategies that are instantly recognisable and long-lasting.',
-		img: '../graphics/Illustration_Branding.svg',
+		img: '/graphics/Illustration_Branding.svg',
 		alt: 'Illustration ',
 	},
 	{
@@ -28,7 +28,7 @@ export default [
 		title: 'Webdevelopment',
 		description:
 			'I enjoy working as part of a team to turn creative ideas into efficient and user-friendly applications. My aim is to work together to develop solutions that work reliably, even with complex requirements.',
-		img: '../graphics/Illustration_Webdev.svg',
+		img: '/graphics/Illustration_Webdev.svg',
 		alt: 'Illustration',
 	},
 	{
@@ -36,7 +36,7 @@ export default [
 		title: 'Illustration & Graphic',
 		description:
 			'Illustrations and graphics add a special touch to any project. With a keen eye for detail and a love of visual storytelling, I create customised illustrations that get to the heart of the message. Whether minimalist, playful or complex - each illustration is tailored precisely to the style and requirements of the project.',
-		img: '../graphics/Illustration_Illustration-Graphics.svg',
+		img: '/graphics/Illustration_Illustration-Graphics.svg',
 		alt: 'Illustration',
 	},
 ];
